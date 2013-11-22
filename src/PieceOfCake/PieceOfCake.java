5b9c6f0563a6f58390650f166dae30c14acd4fe1
Firsto <zaoroman@gmail.com>
2013-11-22 19:56:30 +0700

    YandexContest geom

--- src/PieceOfCake/PieceOfCake.java
+++ src/PieceOfCake/PieceOfCake.java
@@ -47,20 +47,108 @@ package PieceOfCake;
 //        for i:= 1 to n do
 //          s:= s+((y[i+1]+y[i])*(x[i+1]-x[i])/2);
 //        end;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+typedef struct{
+    float x, y;
+} Point2f;
+
+float distance2f(Point2f*, Point2f*);
+float triangleSqare2f(Point2f*, Point2f*, Point2f*);
+float polygonSquare2fv(Point2f*, int);
+
+int main(int argc, char** argv) {
+    srand(time(0));
+
+    int i, n;
+    printf("Enter n: "); scanf("%d", &n);
+
+    Point2f* polygon = (Point2f*)calloc(n, sizeof(Point2f));
+
+    for(i = 0; i < n; i++){
+        printf("Point %d.x: ", i + 1); scanf("%f", &polygon[i].x);
+        printf("Point %d.y: ", i + 1); scanf("%f", &polygon[i].y);
+    }
+
+    printf("Polygon square: %f\n", polygonSquare2fv(polygon, n));
+
+    free(polygon);
+    return 0;
+}
+
+float distance2f(Point2f* p1, Point2f* p2){
+    return sqrtf(pow(p1->x - p2->x, 2) + pow(p1->y - p2->y, 2));
+}
+
+float triangleSqare2f(Point2f* p1, Point2f* p2, Point2f* p3){
+    float
+        a = distance2f(p1, p2),
+        b = distance2f(p2, p3),
+        c = distance2f(p3, p1);
+    float p = (a + b + c) / 2.0;
+
+    return sqrtf(p*(p - a) * (p - b) * (p - c));
+}
+
+float polygonSquare2fv(Point2f* points, int n){
+    float square = 0.0;
+    int i;
+    for(i = 1; i < n - 1; i++)
+        square += triangleSqare2f(points + 0, points + i, points + i + 1);
+
+    return square;
+}
+
+
 */
 
 public class PieceOfCake {
     public static void main(String[] args) {
-        int n = 4;
-        int[] xa = {0, 2, 2, 0};
-        int[] ya = {0, 0, 2, 2};
+//        int n = 6;
+//        int[] x = {2, 3, 2, -1, -2, -1, 0};
+//        int[] y = {5, -1, -4, -4, -2, 3, 0};
+        int n = 6;
+//        int[] x = {-1, 1, 2, 1, -1, -2, 0};
+//        int[] y = {-3, -3, 0, 3, 3, 0,  0};
+        int[] x = {2, -2, -3, 4, 11, 9,  0};
+        int[] y = {5, 4, -1, -4, -2, 3,  0};
+        x[n]=x[0];
+        y[n]=y[0];
 
-        //  s:= s+((y[i+1]+y[i])*(x[i+1]-x[i])/2);
+        double s = S(x, y, n);
+        System.out.println(s);
+
+        int xc = 0;
+        for (int i = 0; i < n-1; i++) {
+            xc += x[i];
+        }
+        xc = xc/n;
+        int yc = 0;
+        for (int i = 0; i < n-1; i++) {
+            yc += y[i];
+        }
+        yc = yc/n;
+        System.out.println("center (" + xc + ", " + yc + ")");
+    }
+
+    public static double S(int[] x, int[] y, int n) {
+        int min = y[0];
+        for (int i = 1; i < n; i++) {
+            if (min > y[i]) min = y[i];
+        }
+        for (int i = 0; i < n+1; i++) {
+            y[i] = y[i]-min;
+        }
 
         double s = 0;
-        for (int i = 0; i < n-1; i++) {
-            s += ((ya[i+1]+ya[i])*(xa[i+1]-xa[i])/2);
+        for (int i = 0; i < n; i++) {
+            s += ((y[i+1]+y[i])*(x[i+1]-x[i])/2);
         }
-        System.out.println(s);
+
+        return s;
     }
 }
