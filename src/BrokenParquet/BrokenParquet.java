6a077e3a170f6bd1138913f09d5dc2d8e4d2399c
Firsto <zaoroman@gmail.com>
2013-12-07 01:36:23 +0700

    YandexContest geom

--- src/BrokenParquet/BrokenParquet.java
+++ src/BrokenParquet/BrokenParquet.java
@@ -35,16 +35,34 @@ public class BrokenParquet {
         for (int u = 0; u < n1; u++) {
             if (findPath(g, u, matching, new boolean[n1]))
                 ++matches;
+//            if (matches>36300)
+//                System.out.println(matches);
         }
         return matches;
     }
 
     public static void main(String[] args) throws Exception {
-
+        try {
         BufferedReader rdr = new BufferedReader(new InputStreamReader(System.in));
         int n=0,m=0,a=0,b=0;
-//        try {
-            String s = rdr.readLine();
+
+            String s = "";
+
+        try {
+            int c;
+
+                StringBuilder sb = new StringBuilder();
+
+                while ((c = rdr.read()) != '\n') {
+                    sb.append((char) c);
+                    s = sb.toString();
+                    s.trim();
+                }
+        } catch (IOException e) {
+            System.exit(0);
+            e.printStackTrace();
+        }
+
         if (s == null) System.exit(0);
             String[] num = s.split("[^\\p{Digit}*]");
 //        System.out.println(Arrays.toString(num));
@@ -56,17 +74,43 @@ public class BrokenParquet {
              a = Integer.parseInt(num[2]);
              b = Integer.parseInt(num[3]);
         } catch(Exception e) {System.exit(0);}
-
+        if (n<1 || m<1 || n>300 || m>300 || a>1000 || b>1000 || a<0 || b<0) System.exit(0);
         char[][] chars = new char[n][m];
         s = "";
 
-            for (int i = 0; i < chars.length; i++) {
-                if ((s = rdr.readLine()) != null) {
-                s.trim();
-                chars[i] = s.toCharArray();
-                chars[i] = Arrays.copyOf(chars[i], m);// chars[i].length > m ? m : chars[i].length);
+
+                try {
+
+                        int c;
+                    for (int i = 0; i < chars.length; i++) {
+                        StringBuilder sb = new StringBuilder();
+                        int l = 0;
+                        while ((c = rdr.read()) != -1) {
+                        if ((char) c == '\n') break;
+                        if (((char) c == '*' || (char) c == '.') && l<m) {
+                            sb.append((char) c);
+                            l++;
+                        }
+                    }
+                        if (l < m) for (int j = 0; j < m - l; j++) sb.append('.');
+                        s = sb.toString();
+                        s.trim();
+//                        s = s.substring(0,m);
+                        chars[i] = s.toCharArray();
+                    }
+                    rdr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (rdr != null) {
+                        try {
+                            rdr.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
-            }
+
             rdr.close();
         int n1 = m*n;
         int n2 = n1;
@@ -91,7 +135,12 @@ public class BrokenParquet {
         int d = maxMatching(g, n2)/2;
         if (2*b>a) System.out.print(d*a+(w-d*2)*b);
         else System.out.print(w * b);
-
+    } catch (StackOverflowError e) {
+        System.err.println("reported recursion level was "+e.getStackTrace().length);
+            System.exit(0);
+        } catch (IOException e) {
+            System.exit(0);
+        }
 //        } catch (Exception e) {
 ////            System.out.println(0);
 ////            System.out.println(e);
