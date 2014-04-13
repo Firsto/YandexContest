635e34f6ae8d54b61a77df7a7c7de5485ac76412
Firsto <zaoroman@gmail.com>
2014-04-13 22:25:17 +0700

    YandexContest — Summer 2014
     FormatParserConverter

--- src/YCS14/FormatsParserConverter.java
+++ src/YCS14/FormatsParserConverter.java
@@ -21,6 +21,7 @@ public class FormatsParserConverter {
         String outputFormat = "";
         String[] fileTree = {""};
         int stringsCount = 0;
+        String[] parentsTree = {""};
 
         BufferedReader rdr = new BufferedReader(new InputStreamReader(System.in));
         try {
@@ -33,6 +34,20 @@ public class FormatsParserConverter {
                     fileTree[i] = rdr.readLine();
                 }
             }
+            if (inputFormat.equals("acm1") || inputFormat.equals("acm2")) {
+                parentsTree = new String[stringsCount];
+                for (int i = 0; i < stringsCount; i++) {
+                    parentsTree[i] = rdr.readLine();
+                }
+            }
+
+            if (inputFormat.equals("acm3")) {
+                parentsTree = new String[stringsCount-1];
+                for (int i = 0; i < stringsCount - 1; i++) {
+                    parentsTree[i] = rdr.readLine();
+                }
+            }
+
             rdr.close();
         } catch (IOException e) {
             System.out.println("Input Error");
@@ -51,11 +66,92 @@ public class FormatsParserConverter {
         }
 
         // Parents detector
+
+        if (inputFormat.equals("python")) {
+            for (Map.Entry<Integer, FileKeeper> fileKeeperEntry : fileSystem.entrySet()) {
+                String name = fileKeeperEntry.getValue().name;
+                int id = fileKeeperEntry.getValue().id;
+                int level = fileKeeperEntry.getValue().loadLevel;
+
+                for (Map.Entry<Integer, FileKeeper> keeperEntry : fileSystem.entrySet()) {
+//                    if (keeperEntry.getValue().parentName != null) continue;
+                    if (keeperEntry.getValue().loadLevel == level+1 && keeperEntry.getValue().id > id) {
+                        keeperEntry.getValue().parentName = name;
+                        keeperEntry.getValue().parentId = id;
+                    }
+                }
+            }
+        }
+
+        if (inputFormat.equals("acm1")) {
+            String[] childIds = {""};
+            int i = 0;
+            for (Map.Entry<Integer, FileKeeper> fileKeeperEntry : fileSystem.entrySet()) {
+                String name = fileKeeperEntry.getValue().name;
+                int id = fileKeeperEntry.getValue().id;
+                int level = fileKeeperEntry.getValue().loadLevel;
+
+                String parents = parentsTree[i];
+                if (parents.length() > 1) {
+                    childIds = parents.split(" ");
+                    for (int j = 1; j <= Integer.parseInt(childIds[0]); j++) {
+                        int childId = Integer.parseInt(childIds[j]);
+                        fileSystem.get(childId).parentId = id;
+                        fileSystem.get(childId).parentName = name;
+//                        fileSystem.get(childId).loadLevel = level + 1;
+                    }
+                }
+//                System.out.println("id " + id + " / level " + fileKeeperEntry.getValue().loadLevel);
+                i++;
+            }
+            for (Map.Entry<Integer, FileKeeper> fileKeeperEntry : fileSystem.entrySet()) {
+                if (fileKeeperEntry.getValue().parentName != null) {
+                    int parentId = fileKeeperEntry.getValue().parentId;
+                    fileKeeperEntry.getValue().loadLevel += fileSystem.get(parentId).loadLevel + 1;
+                }
+            }
+        }
+
+        if (inputFormat.equals("acm2")) {
+            int i = 0;
+            for (Map.Entry<Integer, FileKeeper> fileKeeperEntry : fileSystem.entrySet()) {
+                FileKeeper fk = fileKeeperEntry.getValue();
+                int parentId = Integer.parseInt(parentsTree[i]);
+                fk.parentId = parentId;
+                if (parentId != -1) fk.parentName = fileSystem.get(parentId).name;
+                i++;
+            }
+            for (Map.Entry<Integer, FileKeeper> fileKeeperEntry : fileSystem.entrySet()) {
+                if (fileKeeperEntry.getValue().parentName != null) {
+                    int parentId = fileKeeperEntry.getValue().parentId;
+                    fileKeeperEntry.getValue().loadLevel += fileSystem.get(parentId).loadLevel + 1;
+                }
+            }
+        }
+
+        if (inputFormat.equals("acm3")) {
+            for (int i = 0; i < parentsTree.length; i++) {
+                int parentId = Integer.parseInt(parentsTree[i].split(" ")[0]);
+                int childId = Integer.parseInt(parentsTree[i].split(" ")[1]);
+                FileKeeper fk = fileSystem.get(childId);
+                fk.parentId = parentId;
+                fk.parentName = fileSystem.get(parentId).name;
+            }
+            for (Map.Entry<Integer, FileKeeper> fileKeeperEntry : fileSystem.entrySet()) {
+                if (fileKeeperEntry.getValue().parentName != null) {
+                    int parentId = fileKeeperEntry.getValue().parentId;
+                    fileKeeperEntry.getValue().loadLevel += fileSystem.get(parentId).loadLevel + 1;
+                }
+            }
+        }
+
         for (Map.Entry<Integer, FileKeeper> fileKeeperEntry : fileSystem.entrySet()) {
             FileKeeper fk = fileKeeperEntry.getValue();
-            for (FileKeeper keeper : fileList) {
-                if (fk.parentName != null && fk.parentName.equals(keeper.name) && (fk.loadLevel == keeper.loadLevel+1)) {
-                    fk.parentId = keeper.id;
+
+            for (Map.Entry<Integer, FileKeeper> keeperEntry : fileSystem.entrySet()) {
+                if (fk.parentName != null && fk.parentName.equals(keeperEntry.getValue().name) && (fk.loadLevel == keeperEntry.getValue().loadLevel+1)) {
+                    if (fk.id < keeperEntry.getValue().id) break;
+                    fk.parentId = keeperEntry.getValue().id;
                     break;
                 } else if (fk.parentName == null) {
                     fk.parentId = -1;
@@ -64,15 +160,15 @@ public class FormatsParserConverter {
         }
         for (Map.Entry<Integer, FileKeeper> fileKeeperEntry : fileSystem.entrySet()) {
             FileKeeper fk = fileKeeperEntry.getValue();
-            for (FileKeeper keeper : fileList) {
-                if (fk.id == keeper.parentId) {
+
+            for (Map.Entry<Integer, FileKeeper> keeperEntry : fileSystem.entrySet()) {
+                if (fk.id == keeperEntry.getValue().parentId) {
                     fk.type = FileKeeper.Type.DIR;
                     break;
                 } else {
                     fk.type = FileKeeper.Type.FILE;
                 }
             }
-
 //            System.out.println("NAME: " + fk.name + " / TYPE: " + fk.type.toString() + " / PARENT ID: " + fk.parentId);
         }
 
@@ -148,18 +244,6 @@ public class FormatsParserConverter {
 
         // OUTPUT XML
         if (outputFormat.equals("xml")) {
-//            for (Map.Entry<Integer, FileKeeper> fileKeeperEntry : fileSystem.entrySet()) {
-//                String name = fileKeeperEntry.getValue().name;
-//                int id = fileKeeperEntry.getValue().id;
-//                for (int i = 0; i < fileKeeperEntry.getValue().loadLevel; i++) {
-//                    System.out.print("  ");
-//                }
-//                if (fileKeeperEntry.getValue().type == FileKeeper.Type.DIR) {
-//                    System.out.println("<dir name='" + name + "' id='" + id + "'>");
-//                } else {
-//                    System.out.println("<file name='" + name + "' id='" + id + "'/>");
-//                }
-//            }
             xmlOut(-1);
         }
     }
@@ -216,11 +300,19 @@ public class FormatsParserConverter {
 
         void initialize() {
             if (initialFormat != null && initialFormat != Format.XML) {
-                id = Integer.parseInt(initialInfo.split(" ")[1]);
+                id = Integer.parseInt(initialInfo.trim().split(" ")[1]);
             }
 
             if (initialFormat == Format.FIND) {
                 parseFind();
+            }
+
+            if (initialFormat == Format.PYTHON) {
+                parsePython();
+            }
+
+            if (initialFormat == Format.ACM1 || initialFormat == Format.ACM2 || initialFormat == Format.ACM3) {
+                parseAcm();
             }
         }
 
@@ -237,6 +329,24 @@ public class FormatsParserConverter {
             this.name = cuted[cuted.length-1];
             if (cuted.length > 1) this.parentName = cuted[cuted.length - 2];
 //            System.out.println(this.name + " " + this.id + "   " + this.parentName + "   " + this.parentFullName + "  " + this.loadLevel);
+        }
+
+        void parsePython() {
+            if (initialInfo.startsWith("    ")) {
+                int i = 0;
+                char[] chars = initialInfo.toCharArray();
+                while (chars[i] == ' ') {
+                    i++;
+                }
+                this.loadLevel = i/4;
+            }
+            String[] cuted = initialInfo.trim().split(" ");
+            this.name = cuted[0];
+        }
+
+        void parseAcm() {
+            String[] cuted = initialInfo.trim().split(" ");
+            this.name = cuted[0];
         }
     }
 
